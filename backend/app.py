d45b53b094fbe018a47b25e28e05fe199abb5e3c
16aurora <gursmeep5813@gmail.com>
2025-05-15 19:44:01 +0530

    ready for backend deployment

--- backend/app.py
+++ backend/app.py
@@ -232,3 +232,9 @@ def generate_report():
         all_cves.extend(cves)
 
     return jsonify({"cves": all_cves})
+
+
+if __name__ == "__main__":
+    from os import environ
+    port = int(environ.get("PORT", 5000))
+    app.run(debug=False, host="0.0.0.0", port=port)
